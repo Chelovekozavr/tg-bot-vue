8831010cd31e97a503b3e78ca72d09be85dca767
Anton <a.tsymbal@upswot.com>
2023-03-02 00:46:49 +0200

    hr

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-<!--    <hr>-->
+    <hr>
     <div class="date">
         <h4>{{ matchDate }}</h4>
     </div>
@@ -7,7 +7,7 @@
         <h1>{{ homeTeam }}</h1>
         <h1>{{ awayTeam }}</h1>
     </div>
-<!--    <hr>-->
+    <hr>
 </template>
 
 <script>
@@ -32,31 +32,6 @@ export default {
             homeTeam,
             awayTeam
         }
-        // http://localhost:8080/1?topMatch=true&homeTeam=Динамо&awayTeam=Інгулець&date=01.03.2022&time=14:00
-
-        // function getMatchTitle() {
-        //     title: obj.title,
-        //         time: obj.time,
-        //         date: obj.date
-        //     let urlParams = new URLSearchParams(window.location.search);
-        //     console.log(urlParams.has('matchName'));
-        //     console.log(urlParams.get('matchName'));
-        //
-        //     return urlParams.get('matchName');
-        //     `${urlParams.get('homeTeam')}`
-        // }
-        //
-        // function getMatchDate() {
-        //     let urlParams = new URLSearchParams(window.location.search);
-        //
-        //     return `${urlParams.get('date')} ${urlParams.get('time')}` || '01.01.01 14:00';
-        // }
-        //
-        // onMounted(() => {
-        //     getMatchTitle();
-        //     getMatchDate();
-        // })
-
     }
 }
 </script>
