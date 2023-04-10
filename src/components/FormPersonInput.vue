4364ca6a7293c3a05a52e251b4389e1956c88527
Anton <a.tsymbal@upswot.com>
2023-04-10 16:45:06 +0300

    for firend

--- src/components/FormPersonInput.vue
+++ src/components/FormPersonInput.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="d-flex flex-row flex-gap justify-space-between align-start mb-6">
         <v-label>
-            Стіл бронюю {{
-                forFriend
-            }}
+            Стіл бронюю
+
         </v-label>
+        <h2>{{ forFriend}}</h2>
         <div v-if="!forFriend">
             <v-btn
                 class="ma-2"
